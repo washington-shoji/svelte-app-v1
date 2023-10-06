f071ab27f1b3fc292024892f3b92b65b8c43f1c3
washington-shoji <shoji.washington@gmail.com>
2023-10-07 10:00:08 +1100

    -added table data component

---
+++ src/lib/components/table/TableData.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let data: string;
+	export let extClass = '';
+</script>
+
+<td class="pl-5">
+	<div class="flex items-center">
+		<p class="text-sm leading-none text-gray-600 ml-2 {extClass}">{data}</p>
+	</div>
+</td>
