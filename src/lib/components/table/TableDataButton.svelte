2317c9d4fd1d5bfa30a18ea645e46b13b0b4031c
washington-shoji <shoji.washington@gmail.com>
2023-10-07 10:01:11 +1100

    -added table data button component

---
+++ src/lib/components/table/TableDataButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let title: string;
+	export let icon: string;
+	export let extClass = '';
+</script>
+
+<td class="pl-4">
+	<button
+		class="flex gap-2 items-center hover:ring-2 hover:ring-offset-2 text-sm leading-none text-gray-600 py-3 px-5 bg-gray-100 rounded focus:outline-none {extClass}"
+	>
+		<span class="material-symbols-outlined">{icon}</span>
+		{title}
+	</button>
+</td>
