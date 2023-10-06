34d979de4fb7ad7d1bb00695c3dc2abc2b240ca6
washington-shoji <shoji.washington@gmail.com>
2023-10-07 10:00:55 +1100

    -added table data bold component

---
+++ src/lib/components/table/TableDataBold.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let data: string;
+	export let extClass = '';
+</script>
+
+<td>
+	<div class="flex items-center pl-5">
+		<p class="text-base font-medium leading-none text-gray-700 mr-2 {extClass}">
+			{data}
+		</p>
+	</div>
+</td>
