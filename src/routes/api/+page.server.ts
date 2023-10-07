6ef632e91e80bd1c1416a0055abf5aa941b24dae
washington-shoji <shoji.washington@gmail.com>
2023-10-07 11:23:01 +1100

    -updated dynamic data page server

--- src/routes/api/+page.server.ts
+++ src/routes/api/+page.server.ts
@@ -1,10 +1,20 @@
+import { Api, BaseAPIUrl } from '../../endpoints/api_endpoint';
+
 export const actions = {
-	contactUs: async ({ request }: any) => {
-		const formData = Object.fromEntries(await request.formData());
-		console.warn('formData', formData);
+	addDynamicData: async ({ request }: any) => {
+		const data = Object.fromEntries(await request.formData());
+		const url = `${BaseAPIUrl.Local}/${Api.Dynamic}`;
+		const response = await fetch(url, {
+			method: 'POST',
+			body: JSON.stringify({ data: data }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const result = await response.json();
 		return {
 			success: true,
-			formData
+			result
 		};
 	}
 };
