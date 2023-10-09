3212fc968b901e657553022fa21b5475057b3a14
washington-shoji <shoji.washington@gmail.com>
2023-10-10 08:37:13 +1100

    -updated input component

--- src/lib/components/input/Input.svelte
+++ src/lib/components/input/Input.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	export let impPlaceholder = 'Placeholder';
 	export let formName = 'formName';
 	export let inputValue = '';
@@ -7,6 +7,6 @@
 <input
 	placeholder={impPlaceholder}
 	name={formName}
-	value={inputValue}
+	bind:value={inputValue}
 	class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-blueGray-500 focus:outline-none focus:shadow-outline focus:ring-1 ring-offset-current ring-offset-2 ring-black"
 />
