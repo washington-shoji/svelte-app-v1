add7100b12e8c132ac51dccd2380479152af7384
washington-shoji <shoji.washington@gmail.com>
2023-10-12 08:36:32 +1100

    -updated page server for dynamic data

--- src/routes/api/create/+page.server.ts
+++ src/routes/api/create/+page.server.ts
@@ -1,7 +1,8 @@
-import { Api, BaseAPIUrl } from '../../../endpoints/api_endpoint';
+import type { Actions } from './$types';
+import { BaseAPIUrl, Api } from '../../../endpoints/api_endpoint';
 
 export const actions = {
-	addDynamicData: async ({ request }: any) => {
+	createData: async ({ request }: { request: Request }) => {
 		const data = Object.fromEntries(await request.formData());
 		const url = `${BaseAPIUrl.Local}/${Api.Dynamic}`;
 		const response = await fetch(url, {
@@ -12,9 +13,33 @@ export const actions = {
 			}
 		});
 		const result = await response.json();
-		return {
-			success: true,
-			result
+		if (result) {
+			return {
+				success: true
+			};
+		}
+	},
+	updateData: async ({ request }: { request: Request }) => {
+		const formData = Object.fromEntries(await request.formData());
+		const url = `${BaseAPIUrl.Local}/${Api.Dynamic}/${formData.id}`;
+		const data = {
+			address: formData.address,
+			city: formData.city,
+			name: formData.name,
+			plz: formData.plz
 		};
+		const response = await fetch(url, {
+			method: 'PATCH',
+			body: JSON.stringify({ data: data }),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+		const result = await response.json();
+		if (result) {
+			return {
+				success: true
+			};
+		}
 	}
-};
+} satisfies Actions;
