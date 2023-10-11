bc0e644669c12a3a6676bb70124519f151309ae8
washington-shoji <shoji.washington@gmail.com>
2023-10-12 08:37:25 +1100

    -updated dynamic data page

--- src/routes/api/+page.svelte
+++ src/routes/api/+page.svelte
@@ -1,36 +1,51 @@
 <script lang="ts">
-	import SampleForm from '$lib/components/form/SampleForm.svelte';
+	import { onMount } from 'svelte';
+	import { BaseAPIUrl, Api } from '../../endpoints/api_endpoint';
+	import Table from '$lib/components/table/Table.svelte';
+	import { goto } from '$app/navigation';
 
-	export let form: any;
+	type DataResponse = {
+		id: string;
+		data: any;
+	};
+
+	const apiUrl = `${BaseAPIUrl.Local}/${Api.Dynamic}`;
+	onMount(() => {
+		fetchDynamicData();
+	});
+
+	let response: any;
+	$: dynamicData = [];
+
+	async function fetchDynamicData() {
+		response = await fetch(apiUrl, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then((response) => response.json());
+		dynamicData = response;
+	}
+
+	async function deleteDynamicData(id: string) {
+		await fetch(`${apiUrl}/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).then(() => {
+			fetchDynamicData();
+		});
+	}
+
+	function editData(event: any): void {
+		goto(`/api/create/?id=${event.detail.id}`);
+	}
+
+	function deleteData(event: any): void {
+		const id = event.detail;
+		deleteDynamicData(id);
+	}
 </script>
 
-<SampleForm />
-
-{#if form?.success}
-	<div class="mt-5 bg-white shadow cursor-pointer rounded-xl m-auto md:max-w-3xl">
-		<div class="flex">
-			<div class="flex-1 py-5 pl-5 overflow-hidden">
-				<ul>
-					<li class="text-xs text-gray-600 uppercase">Receiver</li>
-					<li>Name: {form?.formData.name}</li>
-					<li>Address: {form?.formData.address}</li>
-				</ul>
-			</div>
-			<div class="flex-1 py-5 pl-1 overflow-hidden">
-				<ul>
-					<li class="text-xs text-gray-600 uppercase">Sender</li>
-					<li>Plz: {form?.formData.plz}</li>
-					<li>City: {form?.formData.city}</li>
-				</ul>
-			</div>
-			<div class="flex-none pt-2.5 pr-2.5 pl-1">
-				<button
-					type="button"
-					class="px-2 py-2 font-medium tracking-wide text-black capitalize transition duration-300 ease-in-out transform rounded-xl hover:bg-gray-300 focus:outline-none active:scale-95"
-				>
-					<span class="material-symbols-outlined">edit</span>
-				</button>
-			</div>
-		</div>
-	</div>
-{/if}
+<Table tableData={dynamicData} on:edit={editData} on:delete={deleteData} />
