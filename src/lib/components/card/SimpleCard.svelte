e5c83d1cb5c937996dfc84d0364313b6815e80b7
washington-shoji <shoji.washington@gmail.com>
2023-10-14 10:46:29 +1100

    -added props to simple card

--- src/lib/components/card/SimpleCard.svelte
+++ src/lib/components/card/SimpleCard.svelte
@@ -2,9 +2,10 @@
 	export let title = 'Card title';
 	export let info = 'Card info';
 	export let icon = 'info';
+	export let cardColour = 'bg-black';
 </script>
 
-<div class="rounded-md shadow-md bg-black max-w-[180px]">
+<div class="rounded-md shadow-md max-w-[180px] {cardColour}">
 	<div class=" flex items-center gap-3 p-3">
 		<span class="material-symbols-outlined p-2 bg-white rounded-md">{icon}</span>
 		<div class=" text-white">
