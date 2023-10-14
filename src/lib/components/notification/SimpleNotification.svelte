3c9482a39876ccb1a7147a0581ed95dadf6b728f
washington-shoji <shoji.washington@gmail.com>
2023-10-14 12:05:01 +1100

    -added simple notification

---
+++ src/lib/components/notification/SimpleNotification.svelte
@@ -0,0 +1,10 @@
+<div class=" flex justify-between">
+	<div>
+		<h1 class=" text-2xl font-semibold">Dashboard</h1>
+		<p class=" text-xs">All info about the app</p>
+	</div>
+	<div>
+		<span class="material-symbols-outlined p-1 bg-white rounded-md">notifications</span>
+		<span class="material-symbols-outlined p-1 bg-white rounded-md">empty_dashboard</span>
+	</div>
+</div>
