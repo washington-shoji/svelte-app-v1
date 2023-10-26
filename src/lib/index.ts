0cc58bcf34b0b767e951f3c0ec717d3353205372
washington-shoji <shoji.washington@gmail.com>
2023-10-27 07:27:05 +1100

    -added export default for table

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Navbar } from './components/navbar/Navbar.svelte';
+export { default as Table } from './components/table/Table.svelte';
