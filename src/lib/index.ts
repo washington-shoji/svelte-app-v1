e1eebd8c8b9d798e5ca79e30b4353a54f4a01c40
washington-shoji <shoji.washington@gmail.com>
2023-10-27 07:29:54 +1100

    -added export default for table action header

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 // place files you want to import through the `$lib` alias in this folder.
 export { default as Navbar } from './components/navbar/Navbar.svelte';
 export { default as Table } from './components/table/Table.svelte';
+export { default as TableActionHeader } from './components/table/TableActionHeader.svelte';
