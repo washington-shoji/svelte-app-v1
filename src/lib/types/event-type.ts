d892064c44dabf5a62e069addd65b408492627bf
washington-shoji <shoji.washington@gmail.com>
2023-12-31 09:02:02 +1100

    -updated event type

---
+++ src/lib/types/event-type.ts
@@ -0,0 +1,24 @@
+export type EventData = {
+	id?: string;
+	title: string;
+	shortDescription: string;
+	description: string;
+	imageUrl?: string;
+	publicId?: string;
+	date: string;
+	registration: string;
+};
+
+export type EventDataReq = {
+	eventDetails: {
+		id?: string;
+		title: string;
+		shortDescription: string;
+		description: string;
+		imageUrl?: string;
+		publicId?: string;
+		date: string;
+		registration: string;
+	};
+	imageHeader: HTMLInputElement;
+};
