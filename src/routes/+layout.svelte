86f3a59b5570d91f85bd51725f6ad0057e697c2f
washington-shoji <shoji.washington@gmail.com>
2024-01-04 13:59:44 +1100

    -added dashboard sub route layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
-	import Navbar from '$lib/components/navbar/Navbar.svelte';
 	import '../app.css';
 </script>
 
-<div class=" max-h-screen h-screen flex flex-no-wrap">
-	<Navbar />
-
-	<div class=" w-full overflow-y-scroll py-10 px-0 sm:px-6">
-		<slot />
-	</div>
-</div>
+<slot />
