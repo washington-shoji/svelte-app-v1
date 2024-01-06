66fc9250e7f6e3649fcd44c78cbb6a05e5fe0f60
washington-shoji <shoji.washington@gmail.com>
2024-01-06 15:08:22 +1100

    -added text area component

---
+++ src/lib/components/input/TextArea.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let impPlaceholder = 'Placeholder';
+	export let formName = 'formName';
+	export let inputValue = '';
+</script>
+
+<textarea
+	placeholder={impPlaceholder}
+	name={formName}
+	bind:value={inputValue}
+	rows="4"
+	cols="50"
+	class=" text-black placeholder-gray-600 w-full px-4 py-2.5 mt-2 text-base transition duration-500 ease-in-out transform border-transparent rounded-lg bg-gray-200 focus:border-blueGray-500 focus:outline-none focus:shadow-outline focus:ring-1 ring-offset-current ring-offset-2 ring-black"
+/>
