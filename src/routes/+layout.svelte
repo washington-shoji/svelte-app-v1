8ea09cf861e556dede767e76cd7327870e2778e9
washington-shoji <shoji.washington@gmail.com>
2024-01-06 15:06:09 +1100

    -updated public layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
+	import { PublicNavbar } from '$lib';
 	import '../app.css';
 </script>
 
-<slot />
+PublicNavbar
+
+<div class=" max-h-screen h-screen flex flex-no-wrap">
+	<PublicNavbar />
+
+	<div class=" w-full overflow-y-scroll py-10 px-0 sm:px-6">
+		<slot />
+	</div>
+</div>
