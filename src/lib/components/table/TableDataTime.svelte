173e81b88893892ec49a1506ba9c63184c24a0e6
washington-shoji <shoji.washington@gmail.com>
2024-01-07 10:35:28 +1100

    -added table data time component

---
+++ src/lib/components/table/TableDataTime.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let data: string;
+	export let extClass = '';
+
+	const date = new Date(data);
+</script>
+
+<td class="pl-5">
+	<div class="flex items-center">
+		<p class="text-sm leading-none text-gray-600 ml-2 {extClass}">{date.toLocaleDateString()}</p>
+	</div>
+</td>
